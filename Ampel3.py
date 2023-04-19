01d8e22e8891345ce7d7c9f315b9af9ffbaa4b90
craftpi <schpius04@gmail.com>
2023-04-19 18:22:53 +0200

    Fertigstellung umänderung

--- Ampel3.py
+++ Ampel3.py
@@ -15,16 +15,32 @@ def red(a1):
     a1.setaus()
     a1.setan([1,0,0])
 
+def greenf(a1):
+    a1.setaus()
+    a1.setan([0,1])
+
+def redf(a1):
+    a1.setaus()
+    a1.setan([1,0])
+
 #----------------------Variable Erstellung----------------------#
 
-z=int(input("Wie Lange ist die Schaltzeit ? : "))
-a=int(input("Wie viele Ampel benoetigst du ? : "))
+z=int(input("Wie Lange ist die Schaltzeit der Verkehrsampel ? : "))
+zf=int(input("Wie Lange ist die Schaltzeit der Fussgaengerampel ? : "))
+a=int(input("Wie viele Verkehrsampel benoetigst du ? : "))
+b=int(input("Wie viele Fussgaengersampel benoetigst du ? : "))
 
-l=list()
+lv=list()
+lf=list()
+
+for i in range (0, b):
+    fs="fs"+str(i+1)
+    exec(f"a{i} = AmpelFussAnzeige(fs)")
+    lf.append(locals()[f"a{i}"])
 
 for i in range (0, a):
     exec(f"a{i} = AmpelAnzeige(i+1)")
-    l.append(locals()[f"a{i}"])
+    lv.append(locals()[f"a{i}"])
 
 #---------------------Fenster nebeneinander---------------------#
 
@@ -35,22 +51,37 @@ for i in range(0, a):
     x = (window_width + padding) * i
     y = 0
     #(window_height + padding) * i
-    l[i].geometry(f"{window_width}x{window_height}+{x}+{y}")
+    lv[i].geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+for i in range(0, b):
+    window_width = 200
+    window_height = 400
+    padding = 50
+    x = (window_width + padding) * i
+    y = 400
+    #(window_height + padding) * i
+    lf[i].geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 #------------------------Ampel Animation------------------------#
 
 for i in range(0,a):
-    l[i].setaus()
-    l[i].setan([1,0,0])
+    lv[i].setaus()
+    lv[i].setan([1,0,0])
+
+for i in range(0,a):
+    lf[i].setaus()
+    lf[i].setan([1,0,0])
 
 t.sleep(2)
 
 while True:
 
     for i in range(0,a):
-        green(l[i])
+        green(lv[i])
+        greenf(lf[i])
         t.sleep(z)
-        red(l[i])
+        red(lv[i])
+        redf(lf[i])
 
 
 
