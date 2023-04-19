810d6ce5c9dfe8bcd9e6b7b9047ad7942bc81647
craftpi <schpius04@gmail.com>
2023-04-19 12:22:50 +0200

    verbesserungen

--- Ampel2.py
+++ Ampel2.py
@@ -15,6 +15,7 @@ def green(a1):
     t.sleep(1)
     a1.setaus()
     a1.setan([0,0,1 ])
+
 def red(a1):
     a1.setaus()
     a1.setan([0,1,0])
@@ -26,18 +27,32 @@ def red(a1):
 for i in range (0, a):
     exec(f"a{i} = AmpelAnzeige(i+1)")
     l.append(locals()[f"a{i}"])
-    #l.append()
 
 for i in range(0, a):
     window_width = 200
     window_height = 400
     padding = 50
     x = (window_width + padding) * i
-    y = (window_height - padding) * i
+    y = 0
+    #(window_height + padding) * i
     l[i].geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+i=0
+for i in range(0,a):
+    l[i].setaus()
+    l[i].setan([1,0,0])
+
 while True:
+
     for i in range(0,a):
-        red(l[i])
         green(l[i])
+        t.sleep(z)
+        red(l[i])
+        if i!=a-1:
+            red(l[i+1])
+            green(l[i+1])
+        else:
+            red(l[i-1])
+            green(l[i-1])
+    
 
