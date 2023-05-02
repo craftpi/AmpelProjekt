4ccde33cf8a0d789e21c00161a9997cf41554d9c
craftpi <schpius04@gmail.com>
2023-05-02 10:40:18 +0200

    vorbereitungen für geoupscale

--- Ampel4.py
+++ Ampel4.py
@@ -1,4 +1,5 @@
 from AmpelAnzeige import *
+import ctypes
 
 #---------------------Verkehrs Ampel Klasse---------------------#
 
@@ -6,8 +7,10 @@ class Ampel(object):
     def __init__(self,name,i):
         n=str(name)+str(i)
         self.name = AmpelAnzeige(n)
-       # self.rootscreenwidth= Tk().winfo_screenwidth()
+        #self.rootscreenwidth= Tk().winfo_screenwidth()
         #self.rootscreenhight= Tk().winfo_screenheight()
+        user32 = ctypes.windll.user32
+        self.screensize = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
         self.window_width = 200
         self.window_height = 400
         self.padding = 50
@@ -31,11 +34,13 @@ class Ampel(object):
         self.name.setan([1,1,0])
     
     def setgeo(self):
-       self.name.geometry(f"{self.window_width}x{self.window_height}+{self.x}+{self.y}")
-
+        if self.x < self.screensize[0]:
+            self.name.geometry(f"{self.window_width}x{self.window_height}+{self.x}+{self.y}")
+        else:
+            self.name.geometry(f"{self.window_width}x{self.window_height}+{self.x}+{self.y+400}")
     def geoupscalle(self):
-        self.tk.winfo_screenwidth
-        self.tk.winfo_screenheight
+        width=self.screensize[0]
+        hight=self.screensize[1]
 
 #---------------------Fussgänger Ampel Klasse---------------------#
 
